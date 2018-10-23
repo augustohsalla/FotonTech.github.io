d67c227dfa59b6157cc932c9754f605e722459ec
eliabejr <eliabejunior@hotmail.com.br>
2018-10-23 17:38:01 -0300

    Modify php mailer

--- sendemail.php
+++ sendemail.php
@@ -1,30 +1,24 @@
 <?php
-
-// Define some constants
-define( "RECIPIENT_NAME", "Guilherme Jabur" );
-define( "RECIPIENT_EMAIL", "guilhermejabur@outlook.com" );
-
-
-// Read the form values
-$success = false;
-$senderName = isset( $_POST['username'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['username'] ) : "";
-$senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
-$message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
-
-// If all values exist, send the email
-if ( $senderName && $senderEmail && $message) {
-  $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
-  $headers = "From: " . $senderName . " <" . $senderEmail . ">";
-  $msgBody = " Username: " . $senderName . " Email: " . $senderEmail . " Message: " . $message . "";
-  $success = mail( $recipient, $headers, $msgBody );
-
-  //Set Location After Successsfull Submission
-  header('Location: contact.html?message=Successfull');
+$errors = '';
+$myemail = 'guilhermejabur@outlook.com';//<-----Put Your email address here.
+if(empty($_POST['name'])  ||
+   empty($_POST['email']) ||
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
 }
-
-else{
-	//Set Location After Unsuccesssfull Submission
-  	header('Location: contact.html?message=Failed');	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+if( empty($errors))
+{
+$to = $myemail;
+$email_subject = "[$name] New Foton Lead";
+$email_body = "$email_address\n\n$message";
+$headers = "From: $myemail\n";
+$headers .= "Reply-To: $email_address";
+mail($to,$email_subject,$email_body,$headers);
+//redirect to the 'thank you' page
+header('Location: about.html');
 }
-
 ?>
